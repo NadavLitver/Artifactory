44d15e337a12478cb6cfbdaee90188ce4259d5ea
On <ontamam@gmail.com>
2022-10-23 19:10:51 +0300

    making scrips pretty

--- Assets/Scripts/SpikedFrogNoticePlayer.cs
+++ Assets/Scripts/SpikedFrogNoticePlayer.cs
@@ -1,42 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public class SpikedFrogNoticePlayer : State
 {
     SpikedFrogStateHandler handler;
     public override State RunCurrentState()
     {
-        //enlarge eyes/ look at the player stupidley animation? 
-
-       /* if (!GameManager.Instance.assets.PlayerController.GetIsGrounded || !handler.spikedFrogGroundCheck.isGrounded())
+        if (handler.rayData.isPointInBoxButNotInCollider(GameManager.Instance.assets.Player.transform.position))
+        {   //enemy is in range to jump
+            return handler.SpikedFrogJumpToPlayer;
+        }
+        else
         {
-            //wait for the player to land
-            
-            return this; 
-        }*/
-       
-        
-            if(handler.rayData.isPointInBoxButNotInCollider(GameManager.Instance.assets.Player.transform.position))
-            {   //enemy is in range to jump
-                return handler.SpikedFrogJumpToPlayer;
-            }
-            else
-            {
-                //enemy is not in range
-                 return handler.SpikedFrogChase;
-            }
-        
-
-        //return handler.spikedFrogIdle;
+            //enemy is not in range
+            return handler.SpikedFrogChase;
+        }
     }
 
-   
+
 
     void Start()
     {
         handler = GetComponent<SpikedFrogStateHandler>();
     }
 
-  
+
 }
