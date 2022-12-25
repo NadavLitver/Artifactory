3d800f42c0887cbb9811148ea408a172810777e3
On <ontamam@gmail.com>
2022-12-25 15:17:00 +0200

    fixed shroom

---
+++ Assets/Scripts/PlayerCreatureCapturer.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCreatureCapturer : MonoBehaviour
+{
+    //subscrube chance to capture on kill instead of kill
+}
