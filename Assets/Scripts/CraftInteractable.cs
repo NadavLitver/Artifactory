16611f44888e129db55a3a32b717b85d0d743bd8
Nadav Litver <Nlitver@gmail.com>
2023-02-10 20:44:32 +0200

    base tutorial wip

--- Assets/Scripts/CraftInteractable.cs
+++ Assets/Scripts/CraftInteractable.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CraftInteractable : Interactable
 {
     GameObject CraftingPanel => GameManager.Instance.assets.CraftingPanel;
+    public UnityEvent OnInteract;
+    public IProximityDetection m_detection;
     public override void Interact()
     {
         CraftingPanel.SetActive(true);
+        OnInteract?.Invoke();
     }
 }
--- Assets/Scripts/InventoryCraftingPanel.cs
+++ Assets/Scripts/InventoryCraftingPanel.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryCraftingPanel : ItemPanel
 {
     //the panel streched along the right side of the screen. 
     //showing the current inventory status with quantities.
-
+    public List<ItemUiSlot> CreatedSlotsGetter { get => createdSlots; }
+    public List<Button> createdSlotsButtons;
     public override void SubscribeSlots()
     {
         foreach (var item in createdSlots)
         {
             item.OnSelected.AddListener(GameManager.Instance.CraftingManager.SelectInventoryItem);
+            createdSlotsButtons.Add(item.GetComponent<Button>());
         }
     }   
 
