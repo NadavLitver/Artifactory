492c30b696123b2d2ab33df7e70fba37b552c1fb
On <ontamam@gmail.com>
2023-04-18 12:40:20 +0300

    remove relic

--- Assets/Scripts/RelicInventory.cs
+++ Assets/Scripts/RelicInventory.cs
@@ -12,12 +12,32 @@ public class RelicInventory : MonoBehaviour
 
     public void AddRelic(Relic givenRelic)
     {
+        if (RelicList.Contains(givenRelic))
+        {
+            return;
+        }
         relicList.Add(givenRelic);
         GameManager.Instance.assets.prizePanel.CallShowPrizeFromRelic(givenRelic);
         GameManager.Instance.assets.playerActor.RecieveStatusEffects(givenRelic.MyEffectEnum);
         relicBar.AddRelic(givenRelic);
     }
 
+    public void RemoveRelic(Relic givenRelic)
+    {
+        if (!RelicList.Contains(givenRelic))
+        {
+            return;
+        }
+        GameManager.Instance.assets.playerActor.RemoveStatusEffect(GameManager.Instance.generalFunctions.GetStatusFromType(givenRelic.MyEffectEnum));
+        relicList.Remove(givenRelic);
+    }
 
-
+    public void ClearRelics()
+    {
+        foreach (var item in relicList)
+        {
+            RemoveRelic(item);
+        }
+        relicList.Clear();
+    }
 }
