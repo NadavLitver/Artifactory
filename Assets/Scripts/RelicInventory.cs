0e59ca5dc9e37cb0c202c15ce555aa12b697ff95
NadavLitver <Nlitver@gmail.com>
2023-04-18 14:48:55 +0300

    Update RelicInventory.cs

--- Assets/Scripts/RelicInventory.cs
+++ Assets/Scripts/RelicInventory.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RelicInventory : MonoBehaviour
+public class RelicInventory : MonoBehaviour,ISaveable
 {
     [SerializeField] List <Relic> relicList = new List<Relic>();
     [SerializeField] private RelicBar relicBar;
@@ -39,5 +39,43 @@ public class RelicInventory : MonoBehaviour
             RemoveRelic(item);
         }
         relicList.Clear();
+    }
+    public void LoadState(object state)
+    {
+        var saveData = (MySaveData)state;
+
+        if (saveData.relicEnums != null)
+        {
+            for (int i = 0; i < saveData.relicEnums.Count; i++)
+            {
+                Relic current = GameManager.Instance.RelicManager.GetAnyRelicByEnum(saveData.relicEnums[i]);
+                relicList.Add(current);
+                GameManager.Instance.assets.playerActor.RecieveStatusEffects(current.MyEffectEnum);
+                relicBar.AddRelic(current);
+            }
+        }
+    }
+    public object SaveState()
+    {
+        return
+           new MySaveData()
+           {
+               relicEnums = GetListOfEnums()
+           };
+    }
+    public List<StatusEffectEnum> GetListOfEnums()
+    {
+        List<StatusEffectEnum> effectEnums = new List<StatusEffectEnum>();
+        for (int i = 0; i < relicList.Count; i++)
+        {
+            effectEnums.Add(relicList[i].MyEffectEnum);
+        }
+        return effectEnums;
+    }
+
+    [System.Serializable]
+    private struct MySaveData
+    {
+        public List<StatusEffectEnum> relicEnums;
     }
 }
